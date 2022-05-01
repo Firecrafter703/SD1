d2303a84fbae4f21f182e499e74cdd8523778d14
DK703 <91508871+Firecrafter703@users.noreply.github.com>
2022-05-01 16:41:59 -0700

    SD

--- SDCODE.java
+++ SDCODE.java
@@ -1,4 +1,9 @@
 public class SDCODE
 {
+    public static int subtract(int m, int n)
+    {
+    //m-n
+    return m;
+    }
 
 }
---
+++ SDTEST.java
@@ -0,0 +1,14 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class SDTEST {
+    
+
+    @Test
+    public void subtractionTest()
+    {
+        assertEquals(2, SDCODE.subtract(3,1));
+     
+    }
+    
+}
